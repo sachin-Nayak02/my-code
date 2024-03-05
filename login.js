af39193aa226f66dfe712c0149deb2949dbdc132
sachin-Nayak02 <142329486+sachin-Nayak02@users.noreply.github.com>
2024-03-05 16:41:19 +0530

    login.js

---
+++ login.js
@@ -0,0 +1,9 @@
+// let eye =document.getElementById("#eye");
+function show() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
